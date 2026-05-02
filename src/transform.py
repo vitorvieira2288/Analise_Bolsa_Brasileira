7442d3395d56ad1ed7f768f32975dc1c7399c2b4
vitorvieira2288 <139663048+vitorvieira2288@users.noreply.github.com>
2026-05-02 12:29:55 -0300

    Código mais limpo

--- src/transform.py
+++ src/transform.py
@@ -3,6 +3,6 @@ def inspecionar_dados(dados):
     dados.info()
 
 def calcular_retornos(dados):
-    # pct_change gera NaN na primeira linha → removemos
+   
     retornos = dados.pct_change().dropna()
     return retornos
